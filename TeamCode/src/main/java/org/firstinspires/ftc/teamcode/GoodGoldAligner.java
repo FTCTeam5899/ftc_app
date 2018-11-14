badd928fb1a819d6b73db988de6bc29930e12ba5
Radmin <klannen+5899@libertycommon.org>
2018-11-14 15:19:42 -0700

    Enabled the op mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodGoldAligner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodGoldAligner.java
@@ -29,14 +29,35 @@ public class GoodGoldAligner extends AutoSupplies{
         double x = 0;
         double y = 0;
         double times = 0;
+        String order = new String();
         //  Wait until start
         waitForStart();
 
         //pause( 3000 );
         //move(2000,.5,.5);;
+        order = orderDetector.getCurrentOrder().toString();
+
         telemetry.addData("Current Order", orderDetector.getCurrentOrder().toString()); // The current result for the frame
         telemetry.addData("Last Order", orderDetector.getLastOrder().toString());
         telemetry.update();
+        move(800,0.4,0.4);
+        if(order.equals("LEFT")){
+            move(600, -0.3, 0.3);
+        }
+        else if(order.equals("RIGHT")){
+            move(600, 0.3, -0.3);
+        }
+        else if(order.equals("Center")){
+
+        }
+        else{
+            while(runtime.milliseconds()<20000){
+                telemetry.clear();
+                telemetry.addData("Error Code", 1);
+                telemetry.update();
+            }
+        }
+            move(1700, 0.5, 0.5);
         //  Turn all motors off and sleep
         motorFwdLeft.setPower(0);
         motorFwdRight.setPower(0);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class ServoTest extends LinearOpMode {
+
+    private CRServo servo;
+
+    @Override
+    public void runOpMode() {
+
+        servo = hardwareMap.get(CRServo.class, "servo");
+        double max = 1;
+        double left = 0;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            left = this.gamepad1.left_stick_y;
+
+            if(this.gamepad1.x){max = 0.3;}
+            if(this.gamepad1.y){max = 0.5;}
+            if(this.gamepad1.b){max = 0.8;}
+            if(this.gamepad1.a){max = 1.0;}
+
+            servo.setPower(left * max);
+
+            telemetry.addData("Max Speed", max);
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+}
